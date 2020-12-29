9cf529900ff80aeb5359f0bad8141ada96abd214
tengfei8771 <532050072@qq.com>
2020-12-29 14:38:59 +0800

    1.修改

--- WebApi/Controllers/HealthCheckController.cs
+++ WebApi/Controllers/HealthCheckController.cs
@@ -11,6 +11,10 @@ namespace WebApi.Controllers
     [ApiController]
     public class HealthCheckController : ControllerBase
     {
+        /// <summary>
+        /// 健康检查接口
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult HealthCheck()
         {
